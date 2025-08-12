69dd953548adf501ee998aa901b1228434f1fb85
Marcelo Vieira <vieiramarcelo.vds@gmail.com>
2025-08-12 17:37:20 -0300

    estilizando com css, adicionando comentarios e adicionando readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-"# funcionarios" 
+# EXIBINDO INFORMAÇÕES IMPORTADAS DE UM ARQUIVO JSON
+
+![](./img/preview.PNG)
+
+Importando dados de um arquivo json de maneira local e adicionando elementos na página com JavaScript.
+
+## Tecnologias
+* HTML
+* CSS
+* JavaScript
+* JSON
+
+## Autor
+[Marcelo Vieira](<https://www.linkedin.com/in/marcelovieirasilva/>)
--- main.js
+++ main.js
@@ -5,22 +5,27 @@ import funcionarios from "./funcionarios.json" with {type: "json"}
 
 function criarCards(funcionario){
 
+    //sessao principal (main) do html que ja existe
+    const sessaoPrincipal = document.getElementById('sessaoPrincipal')
+    //adicionando com js um elemento novo (div)
     const cardFuncionarios = document.createElement('div')
 
-    const sessaoPrincipal = document.getElementById('sessaoPrincipal')
-    
+    // adicionando a criação dos 3 elementos que o card vai possuir em constantes(ou variaveis)
     const imagem = document.createElement('img')
     const nome = document.createElement('span')
-    const cargo = document.createElement('span')
+    const cargo = document.createElement('p')
 
-    imagem.src = `./img/ana.jpg`
+    //informando a source da imagem e o texto dos outros 2 elementos, todas informações sendo retiradas do json que foi importado
+    imagem.src = `./img/${funcionario.imagem}`
     nome.textContent = funcionario.nome
     cargo.textContent = funcionario.cargo
     
+    //adicionando os 3 elementos em cada div
     cardFuncionarios.appendChild(imagem)
     cardFuncionarios.appendChild(nome)
     cardFuncionarios.appendChild(cargo)
 
+    //colocando as divs dentro de main
     sessaoPrincipal.appendChild(cardFuncionarios)
 
 }
