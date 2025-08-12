5c35814dade71aa725e61a4acbc2d6cdff5e1813
MARCELO VIEIRA DA SILVA <25132416@CFP127ALUNO.LOCAL>
2025-08-12 11:25:20 -0300

    corrigindo erros

--- main.js
+++ main.js
@@ -6,18 +6,22 @@ import funcionarios from "./funcionarios.json" with {type: "json"}
 function criarCards(funcionario){
 
     const cardFuncionarios = document.createElement('div')
+
+    const sessaoPrincipal = document.getElementById('sessaoPrincipal')
     
     const imagem = document.createElement('img')
     const nome = document.createElement('span')
     const cargo = document.createElement('span')
 
-    imagem.src = funcionario.imagem
+    imagem.src = `./img/ana.jpg`
     nome.textContent = funcionario.nome
     cargo.textContent = funcionario.cargo
     
     cardFuncionarios.appendChild(imagem)
     cardFuncionarios.appendChild(nome)
     cardFuncionarios.appendChild(cargo)
+
+    sessaoPrincipal.appendChild(cardFuncionarios)
 
 }
 
